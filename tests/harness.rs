841670f4ee5fe1a0b4cf9f784e5790e723261567
SwayStar123 <swayambhanded@gmail.com>
2024-05-30 13:27:48 +0530

    update versions, use fuel-toolchain fuel-core

--- tests/harness.rs
+++ tests/harness.rs
@@ -1,4 +1,7 @@
-use fuels::{prelude::*, types::{Bits256, ContractId}};
+use fuels::{
+    prelude::*,
+    types::{Bits256, ContractId},
+};
 
 // Load abi from json
 abigen!(
@@ -66,7 +69,9 @@ async fn test_proxy() {
     let (_target_instance, target_id) = get_contract_instance(wallet.clone()).await;
     let (proxy_instance, _proxy_id) = get_proxy_contract_instance(wallet, target_id).await;
 
-    let b256_1 = Bits256::from_hex_str("0x00000000000000000000000059F2f1fCfE2474fD5F0b9BA1E73ca90b143Eb8d0").unwrap();
+    let b256_1 =
+        Bits256::from_hex_str("0x00000000000000000000000059F2f1fCfE2474fD5F0b9BA1E73ca90b143Eb8d0")
+            .unwrap();
 
     let result = proxy_instance
         .methods()
@@ -85,9 +90,10 @@ async fn test_proxy2() {
     let wallet = get_wallet().await;
     let (_target_instance, target_id) = get_contract_instance(wallet.clone()).await;
     let (proxy_instance, _proxy_id) = get_proxy_contract_instance(wallet, target_id).await;
-    let b256_2 = Bits256::from_hex_str("0x0000000000000000000000001111111111111111111111111111111111111111").unwrap();
+    let b256_2 =
+        Bits256::from_hex_str("0x0000000000000000000000001111111111111111111111111111111111111111")
+            .unwrap();
 
-    
     let result = proxy_instance
         .methods()
         .test_function_2()
@@ -99,4 +105,3 @@ async fn test_proxy2() {
 
     assert_eq!(result, b256_2);
 }
-
